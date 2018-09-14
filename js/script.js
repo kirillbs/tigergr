ac253d318ac55a8f940f082b01f592cc0488c3f2
Kirill <kb@gmail.com>
2018-09-14 17:13:19 +0300

    3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 $(document).ready(function(){
+    $('.modal-faq-close').on('click', function () {
+       $('.modal-faq').css('display', 'none');
+    });
+    $('.faq-sect a').on('click', function (e) {
+        e.preventDefault();
+        $('.modal-faq-content').html($(this).siblings('.faq-modal-link-content').eq(0).html());
+        $('.modal-faq').css('display', 'block');
+    });
+    $('.collapse-menu').on('click', function () {
+       $('.ref-menu').addClass('hidden');
+        $(this).css('display', 'none');
+        $('.uncollapse').css('display', 'block');
+    });
+    $('.uncollapse').on('click', function () {
+        $('.ref-menu').removeClass('hidden');
+        $(this).css('display', 'none');
+        setTimeout(function () {
+            $('.collapse-menu').css('display', 'block');
+        }, 200)
+    });
 });
